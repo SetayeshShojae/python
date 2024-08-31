96061404e0db3c02df675be4b449d26a7d15b1ad
setayeshshojae <setayeshshojae05@gmail.com>
2024-08-31 18:23:02 +0430

    first commit

---
+++ jalase1/tamrin _1.py
@@ -0,0 +1,9 @@
+
+try:
+        age =int(input("lotfan sen khod ra vared konid?"))
+        if age >=18:
+                  print("to mitpny ray bedy:)")
+        else:
+                 print("to nemitony ray bedy:(")
+except ValueError:
+        print("age moshkely pish omad")
---
+++ jalase1/tamrin_2.py
@@ -0,0 +1,15 @@
+import random
+number = random.randint(1,100)
+
+while True :
+    try:
+        hads=int(input("adad khod ra vared konid"))
+        if number >= hads:
+            print("adad bozorg tare")
+        elif number <=hads:
+            print("adad kochek tare")
+        else:
+            print("dorost hads zady:)")
+        break
+    except ValueError :
+        print("adad sahih vared kon")
