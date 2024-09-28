6c21a582dd30c931b92e7f115f6d6afb11045d28
setayeshshojae <setayeshshojae05@gmail.com>
2024-09-28 17:25:48 +0330

    commit

--- jalase3/tamrin_5.py
+++ jalase3/tamrin_5.py
@@ -1,11 +1,1 @@
-def benoulli_numbers(tedad):
-    benoulli_list = [1]
-    for d in range (1 , tedad + 1):
-        benoulli_list . append(0)
-        for s in range(d ,0, -1):
-            benoulli_list[s - 1] = j * (benoulli_list[s - 1] - benoulli_list[s] )
-        return benoulli_list
-tedad = int(input("tedad adad donbahe bernoulli ra vared kon:"))
-benoulli_sequence = benoulli_numbers(tedad)
-print("donbale bernoulli ta adad" , tedad ,"ebarat ast az:")
-print(benoulli_sequence)
+print(hello)
