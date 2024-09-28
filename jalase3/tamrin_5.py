f20f8e458409f1fda6fc6ae8e408d83ab0ca1994
setayeshshojae <setayeshshojae05@gmail.com>
2024-09-28 17:06:26 +0330

    commit

--- jalase3/tamrin_5.py
+++ jalase3/tamrin_5.py
@@ -1,9 +1,9 @@
 def benoulli_numbers(tedad):
     benoulli_list = [1]
-    for m in range (1 , tedad + 1):
+    for d in range (1 , tedad + 1):
         benoulli_list . append(0)
-        for j in range(m ,0, -1):
-            benoulli_list[j - 1] = j * (benoulli_list[j - 1] - benoulli_list[j] )
+        for s in range(d ,0, -1):
+            benoulli_list[s - 1] = j * (benoulli_list[s - 1] - benoulli_list[s] )
         return benoulli_list
 tedad = int(input("tedad adad donbahe bernoulli ra vared kon:"))
 benoulli_sequence = benoulli_numbers(tedad)
---
+++ jalase3/tamrin_7.py
@@ -0,0 +1,3 @@
+adadha = [int(x) for x in input("plaase_enter_your_list:(plase_separate_numbers_with_spaces) ").split()] 
+adad= list(map(lambda x: x**2, adadha)) 
+print("list_of_powers_of_two", adad)
